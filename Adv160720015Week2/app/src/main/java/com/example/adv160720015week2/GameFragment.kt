cfd4bd825d0585d5dad1f53c14c8e1a48a0dddee
aaronmacauley <89644550+aaronmacauley@users.noreply.github.com>
2023-09-06 01:58:16 +0700

    Tugas Week 2

--- Adv160720015Week2/app/src/main/java/com/example/adv160720015week2/GameFragment.kt
+++ Adv160720015Week2/app/src/main/java/com/example/adv160720015week2/GameFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-
+import androidx.navigation.Navigation
+import kotlin.random.Random
 class GameFragment : Fragment() {
-
+    var total =0;
+    var point = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,12 +24,44 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val btnAnswer = view.findViewById<Button>(R.id.btnAnswer);
+        val num1 = view.findViewById<TextView>(R.id.txtNumber1);
+        val num2= view.findViewById<TextView>(R.id.txtNumber2);
 
+
+
+        fun generateRandomNumbers() {
+            val randomNum1 = Random.nextInt(1, 20)
+            val randomNum2 = Random.nextInt(1, 100)
+            num1.text = randomNum1.toString()
+            num2.text = randomNum2.toString()
+            total = randomNum1 + randomNum2
+
+        }
+        generateRandomNumbers()
         if (arguments!=null){
             val playerName = GameFragmentArgs.fromBundle(requireArguments()).playerName
             val txtTurn = view.findViewById<TextView>(R.id.txtTurn)
             txtTurn.text= "$playerName's turn";
         }
+        btnAnswer.setOnClickListener{
+            val randomNum1 = Random.nextInt(1, 20)
+            val randomNum2 = Random.nextInt(1, 10)
+            val txtAnswer = view.findViewById<EditText>(R.id.txtAnswer);
+            val answerText = txtAnswer.text.toString()
+            val answer = answerText.toIntOrNull()
+            if (answer == total) {
+                point++
+
+            }
+            else{
+                val action = GameFragmentDirections.actionResultFragment(point)
+                Navigation.findNavController(it).navigate(action)
+            }
+
+            generateRandomNumbers()
+        }
+
 
     }
 }
