3428de397bbaf57745bdefb1a5977293ada9d44d
aaronmacauley <89644550+aaronmacauley@users.noreply.github.com>
2023-11-06 12:07:22 +0700

    room1

---
+++ ToDoApp/app/src/main/java/com/example/todoapp/viewmodel/ListTodoViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.todoapp.viewmodel
+
+import android.app.Application
+import android.widget.TextView
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.room.Room
+import com.example.todoapp.model.Todo
+import com.example.todoapp.model.TodoDatabase
+import com.example.todoapp.view.TodoListAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+class ListTodoViewModel(application: Application) : AndroidViewModel(application), CoroutineScope {
+    val todoLD = MutableLiveData<List<Todo>>()
+    val todoLoadErrorLD = MutableLiveData<Boolean>()
+    val loadingLD = MutableLiveData<Boolean>()
+
+    private var job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.IO
+
+    fun refresh() {
+        loadingLD.value = true
+        todoLoadErrorLD.value = false
+        launch {
+            val db = Room.databaseBuilder(
+                getApplication(),
+                TodoDatabase::class.java, "newtododb").build()
+
+            val allTodos = db.todoDao().selectAllTodo()
+            todoLD.postValue(allTodos)
+        }
+    }
+
+    fun clearTask(todo: Todo) {
+        launch {
+            val db = Room.databaseBuilder(
+                getApplication(),
+                TodoDatabase::class.java, "newtododb").build()
+            db.todoDao().deleteTodo(todo)
+
+            val allTodos = db.todoDao().selectAllTodo()
+            todoLD.postValue(allTodos)
+        }
+    }
+
+
+}
